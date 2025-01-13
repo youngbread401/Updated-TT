0d114f18d2af8ef472479f780ebcf80842bcf0ae
Daniel <daniel.a.lakin@gmail.com>
2025-01-13 15:06:35 -0500

    Simplify build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
   plugins: [react()],
-  define: {
-    'process.env': {}
+  build: {
+    outDir: 'dist'
   },
   resolve: {
     alias: {
-      'react-native': 'react-native-web',
-      '@': resolve(__dirname, 'src')
-    },
-    extensions: ['.web.js', '.js', '.jsx', '.json']
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    commonjsOptions: {
-      transformMixedEsModules: true
+      'react-native': 'react-native-web'
     }
-  },
-  optimizeDeps: {
-    include: ['react-native-web']
-  },
-  server: {
-    port: 3000,
-    open: true
   }
 });
